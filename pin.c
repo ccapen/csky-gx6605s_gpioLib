f7cbbe7f0423bbd263e92dd6099b5a3a21514132
asd <abkt3@qq.com>
2021-01-16 16:35:33 +0800

    add readPin() and fix macro defintions

--- pin.c
+++ pin.c
@@ -4,7 +4,8 @@ int openPin(int PinNum,Direction dir){
 	char sf[100];
 	if(PinNum>13||PinNum<5) return -1;
 	fd=open("/sys/class/gpio/export",O_WRONLY);
-	write(fd,PinNum,sizeof(int);
+	sprintf(sf,"%d",PinNum);
+	write(fd,sf,sizeof(int));
 	close(fd);
 	sprintf(sf,"/sys/class/gpio/gpio%d/direction",PinNum);
 	fd=open(sf,O_WRONLY);
@@ -15,10 +16,15 @@ int openPin(int PinNum,Direction dir){
 	close(fd);
 	return 0;
 }
-int setPin(int PinNum,bool val){
+int setPin(int PinNum,int val){
 	int fd;
 	char sf[100];
 	if(PinNum>13||PinNum<5) return -1;
+	sprintf(sf,"/sys/class/gpio/gpio%d/direction",PinNum);
+	fd=open(sf,O_RDONLY);
+	read(fd,sf,2);
+	close(fd);
+	if(sf=="in") return -1;
 	sprintf(sf,"/sys/class/gpio/gpio%d/value",PinNum);
 	fd=open(sf,O_WRONLY);
 	if(val)
@@ -28,11 +34,24 @@ int setPin(int PinNum,bool val){
 	close(fd);
 	return 0;
 }
+int readPin(int PinNum){
+	int fd;
+	char sf[100],val;
+	if(PinNum>13||PinNum<5) return -1;
+	sprintf(sf,"/sys/class/gpio/gpio%d/value",PinNum);
+	fd=open(sf,O_RDONLY);
+	read(fd,&val,1);
+	close(fd);
+	if(val=='0') return 0;
+	else return 1;
+}
 int closePin(int PinNum){
 	int fd;
+	char sf[10];
 	if(PinNum>13||PinNum<5) return -1;
 	fd=open("/sys/class/gpio/unexport",O_WRONLY);
-	write(fd,PinNum,sizeof(int);
+	sprintf(sf,"%d",PinNum);
+	write(fd,sf,sizeof(int));
 	close(fd);
 	return 0;
 }
--- pin.h
+++ pin.h
@@ -1,17 +1,21 @@
+#ifndef CSKY_GX6605S_GPIOLIB_
+#define CSKY_GX6605S_GPIOLIB_
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<stdio.h>
-#include<stdlib.h>
-enum Direction{
+typedef enum{
 	in,
 	out
-};
+}Direction;
 /*open pin(5 to 13),return 0 when success,-1 when fail*/
 int openPin(int PinNum,Direction dir);
 /*set value of pin(5 to 13),return 0 when success,-1 when fail*/
-int setPin(int PinNum,bool val);
+int setPin(int PinNum,int val);
+/*read pin(5 to 13),return value when success,-1 when fail*/
+int readPin(int PinNum);
 /*close pin(5 to 13),return 0 when success,-1 when fail*/
 int closePin(int PinNum);
+#endif
 
